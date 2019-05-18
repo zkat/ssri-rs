8626e01d705340dbc2f07c1479d6c502c7fdee79
Kat Marchán <kzm@zkat.tech>
2019-05-18 14:07:40 -0700

    docs: add initial docs and README

---
+++ README.md
@@ -0,0 +1,4 @@
+[`ssri`](https://github.com/zkat/ssri-rs), short for Standard Subresource
+Integrity, is a Rust library for parsing, manipulating, serializing,
+generating, and verifying [Subresource Integrity](https://w3c.github.io/webappsec/specs/subresourceintegrity/)
+hashes.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,42 @@
+/*!
+[`ssri`](https://github.com/zkat/ssri-rs), short for Standard Subresource
+Integrity, is a Rust library for parsing, manipulating, serializing,
+generating, and verifying [Subresource Integrity](https://w3c.github.io/webappsec/specs/subresourceintegrity/)
+hashes.
+
+# Usage
+
+Parsing and serializing Subresource Integrity strings:
+```no_run
+let integrity = String::from("sha512-9KhgCRIx/AmzC8xqYJTZRrnO8OW2Pxyl2DIMZSBOr0oDvtEFyht3xpp71j/r/pAe1DM+JI/A+line3jUBgzQ7A==");
+
+let parsed: ssri::Integrity = integrity.parse().unwrap();
+assert_eq!(parsed.to_string(), integrity)
+```
+
+Generating a new hash from file data:
+```no_run
+# use ssri::{Algorithm, Integrity};
+# use std::fs::File;
+# use std::io::prelude::*;
+// Get file data...
+let mut file = File::open("foo.txt").unwrap();
+let mut contents = String::new();
+file.read_to_string(&mut contents).unwrap();
+
+// Hand it to ssri.
+let mut hasher = Integrity::new();
+hasher.algorithm(Algorithm::Sha256);
+hasher.input(&contents);
+
+let sri = hasher.result();
+
+assert_eq!(sri.to_string(), "sha256-deadbeef");
+
+// Verify the data:
+assert_eq!(sri.check(&contents).unwrap(), Algorithm::Sha256);
+```
+*/
 mod algorithm;
 mod hash;
 mod integrity;
