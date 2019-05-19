74d75d0ebdb22c19bf247114ce433471aa4540e6
Kat Marchán <kzm@zkat.tech>
2019-05-19 12:06:33 -0700

    feat(builder): implemented first iteration of Builder

--- src/builder.rs
+++ src/builder.rs
@@ -1,27 +1,80 @@
 use crate::algorithm::Algorithm;
+use crate::hash::Hash;
 use crate::integrity::Integrity;
 
+extern crate sha1;
+use sha2;
+use digest::Digest;
+use base64;
+
 #[derive(Clone)]
-pub struct Builder {}
+enum Hasher {
+    Sha1(sha1::Sha1),
+    Sha256(sha2::Sha256),
+    Sha384(sha2::Sha384),
+    Sha512(sha2::Sha512),
+}
+
+#[derive(Clone)]
+pub struct Builder {
+    hashers: Vec<Hasher>,
+    disturbed: bool
+}
+
 impl Builder {
     pub fn new() -> Builder {
-        Builder {}
+        Builder { hashers: vec!(), disturbed: false }
     }
     pub fn algorithm(&mut self, algo: Algorithm) {
-        unimplemented!()
+        if self.disturbed {
+            panic!("Can't add new algorithms if Builder::input() has already been called");
+        }
+        self.hashers.push(match algo {
+            Algorithm::Sha1 => Hasher::Sha1(sha1::Sha1::new()),
+            Algorithm::Sha256 => Hasher::Sha256(sha2::Sha256::new()),
+            Algorithm::Sha384 => Hasher::Sha384(sha2::Sha384::new()),
+            Algorithm::Sha512 => Hasher::Sha512(sha2::Sha512::new()),
+        })
     }
     // This is Digest::Input
     pub fn input<B: AsRef<[u8]>>(&mut self, input: B) {
-        unimplemented!()
-    }
-    pub fn chain<B: AsRef<[u8]>>(&mut self, data: B) -> Self {
-        unimplemented!()
+        self.disturbed = true;
+        for hasher in self.hashers.iter_mut() {
+            match hasher {
+                Hasher::Sha1(h) => h.input(&input),
+                Hasher::Sha256(h) => h.input(&input),
+                Hasher::Sha384(h) => h.input(&input),
+                Hasher::Sha512(h) => h.input(&input),
+            }
+        }
     }
     // Digest::Reset
     pub fn reset(&mut self) {
-        unimplemented!()
+        self.hashers = vec!();
+        self.disturbed = false;
     }
-    pub fn result(&mut self) -> Integrity {
-        unimplemented!()
+    pub fn result(self) -> Integrity {
+        let mut hashes = self.hashers.into_iter().map(|h| {
+            let (algorithm, data) = match h {
+                Hasher::Sha1(h) => (
+                    Algorithm::Sha1, base64::encode(&h.result())
+                ),
+                Hasher::Sha256(h) => (
+                    Algorithm::Sha256, base64::encode(&h.result())
+                ),
+                Hasher::Sha384(h) => (
+                    Algorithm::Sha384, base64::encode(&h.result())
+                ),
+                Hasher::Sha512(h) => (
+                    Algorithm::Sha512, base64::encode(&h.result())
+                ),
+            };
+            Hash {
+                algorithm,
+                digest: data
+            }
+        }).collect::<Vec<Hash>>();
+        hashes.sort_unstable();
+        Integrity { hashes }
     }
 }
