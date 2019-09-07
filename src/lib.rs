9dd2b3a97cf04266a9d99246cc8dcf2db2a6b632
Kat Marchán <kzm@zkat.tech>
2019-09-06 21:42:51 -0700

    feat(license): relicense under MPL-2.0 (#2)
    BREAKING CHANGE: The license terms have changed. Please refer to LICENSE in the root of this repository for details. Future use of this library may require release of source code for any changes applied to it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+
 /*!
 [`ssri`](https://github.com/zkat/ssri-rs), short for Standard Subresource
 Integrity, is a Rust library for parsing, manipulating, serializing,
