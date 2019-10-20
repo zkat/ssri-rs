a58a504626fecef495224a8bf734cfeeafb92d4a
Kat Marchán <kzm@zkat.tech>
2019-10-20 17:17:58 -0400

    docs: add some docs to errors

--- src/errors.rs
+++ src/errors.rs
@@ -2,10 +2,13 @@ use thiserror::Error;
 
 use crate::Integrity;
 
+/// Integrity-related error values.
 #[derive(Error, Debug)]
 pub enum Error {
+    /// Error parsing an SRI string into an Integrity object.
     #[error("Failed to parse subresource integrity string: {0}")]
     ParseIntegrityError(String),
+    /// Error matching two Integrity values.
     #[error("Integrity check failed.\n\tWanted: {0}\n\tActual: {1}")]
     IntegrityCheckError(Integrity, Integrity),
 }
