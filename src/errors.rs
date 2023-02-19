c2071f6ab74ff89448b28fd016f2f090479d44a5
Kat Marchán <kzm@zkat.tech>
2023-02-18 21:21:37 -0800

    feat(miette): Add miette and derive Diagnostic for Error

--- src/errors.rs
+++ src/errors.rs
@@ -1,14 +1,17 @@
+use miette::Diagnostic;
 use thiserror::Error;
 
 use crate::Integrity;
 
 /// Integrity-related error values.
-#[derive(Error, Debug, Clone, PartialEq, Eq)]
+#[derive(Diagnostic, Error, Debug, Clone, PartialEq, Eq)]
 pub enum Error {
     /// Error parsing an SRI string into an Integrity object.
     #[error("Failed to parse subresource integrity string: {0}")]
+    #[diagnostic(code(ssri::parse_integrity_error), url(docsrs))]
     ParseIntegrityError(String),
     /// Error matching two Integrity values.
     #[error("Integrity check failed.\n\tWanted: {0}\n\tActual: {1}")]
+    #[diagnostic(code(ssri::integrity_check_error), url(docsrs))]
     IntegrityCheckError(Integrity, Integrity),
 }
